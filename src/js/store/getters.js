014ec229491828e76c74a5ca10a394928a4bba62
J. Hite Billes <hite@billes.io>
2016-10-12 10:32:23 -0500

    Added error handling for when cart item is not found in the store

--- src/js/store/getters.js
+++ src/js/store/getters.js
@@ -3,18 +3,33 @@ export const allProducts = state => state.products.all
 
 // get all products in cart
 export const cartProducts = state => {
+	// map cart items to products
 	return state.cart.added.map(({ id, quantity }) => {
+		// if there are products in the store
+		// find the product
 		if (state.products.all.length) {
 			const product = state.products.all.find(product => product.id === id)
-			return {
-				id: product.id,
-				title: product.title,
-				price: product.price,
-				inventory: product.inventory,
-				quantity
+			
+			// if the cart item is in the product store
+			// return the product
+			if (product) {
+				return {
+					id: product.id,
+					title: product.title,
+					price: product.price,
+					inventory: product.inventory,
+					quantity
+				}
+			} else {
+				// if no product found return an empty object
+				return {}
 			}
 		} else {
+			// if no products in the store
+			// return an empty object
 			return {}
 		}
 	})
+	// remove the empty objects from the array
+	.filter(item => Object.keys(item).length !== 0)
 }
